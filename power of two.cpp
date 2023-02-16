df3e68e3d76a55cb17e63e2d51473c82733ce0ef
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-02-16 12:13:04 +0600

    Update power of two.cpp

--- power of two.cpp
+++ power of two.cpp
@@ -13,3 +13,18 @@ public:
         return n == 1;
     }
 };
+
+// using bit manupulation
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n > 0 && (n & (n - 1)) == 0){
+            return true;
+        }
+        return false;
+    }
+};
+// time and space: O(1)
